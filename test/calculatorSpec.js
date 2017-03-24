809eca50b097c5d2b810847a0cc6f667ab56d991
Artem Govorov <artem.govorov@gmail.com>
2017-03-24 11:57:49 +1000

    Winston example

--- test/calculatorSpec.js
+++ test/calculatorSpec.js
@@ -1,10 +1,12 @@
 'use strict';
 
 let calculator = require('../lib/calculator');
+let winston = require('winston');
 
 describe('calculator', () => {
 
   it('should add numbers', () => {
+    winston.verbose(calculator.add(3, 5));
     expect(calculator.add(2, 2)).to.equal(4);
   });
 });
--- wallaby.js
+++ wallaby.js
@@ -10,6 +10,21 @@ module.exports = function () {
 
     setup: function () {
       global.expect = require('chai').expect;
+
+      var winston = require('winston');
+      if (!winston.transports.ConsoleLogger) {
+        var ConsoleLogger = winston.transports.ConsoleLogger = function (options) {
+          this.level = 'verbose';
+        };
+        require('util').inherits(ConsoleLogger, winston.Transport);
+        ConsoleLogger.prototype.log = function (level, msg, meta, callback) {
+          console.log(msg);
+          callback(null, true);
+        };
+        winston.remove(winston.transports.Console);
+        winston.add(winston.transports.ConsoleLogger);
+      }
+
     },
 
     env: {
