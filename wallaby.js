717c3074dd5cc6622d70d10d960a700fb21c220c
Artem Govorov <artem.govorov@gmail.com>
2017-04-05 10:19:00 +1000

    Initial commit

--- wallaby.js
+++ wallaby.js
@@ -10,6 +10,21 @@ module.exports = function () {
 
     setup: function () {
       global.expect = require('chai').expect;
+
+      var mocha = wallaby.testFramework;
+
+      // setup sinon hooks
+      var sinon = require('sinon');
+      mocha.suite.beforeEach('sinon before', function() {
+        if (null == this.sinon) {
+          this.sinon = sinon.sandbox.create();
+        }
+      });
+      mocha.suite.afterEach('sinon after', function() {
+        if (this.sinon && 'function' === typeof this.sinon.restore) {
+          this.sinon.restore();
+        }
+      });
     },
 
     env: {
