0f07fa39e5ec2213bf91afa81106fb0821f0fd90
Artem Govorov <artem.govorov@gmail.com>
2017-04-08 14:32:55 +1000

    Initial commit

--- wallaby.js
+++ wallaby.js
@@ -1,20 +1,65 @@
-module.exports = function () {
+module.exports = function (wallaby) {
   return {
     files: [
-      'lib/**/*.js'
+      {pattern: 'config/**',instrument: false, load: true, ignore: false},
+      {pattern: 'config/passport.js',instrument: false, load: true, ignore: false},
+      {pattern: 'app.js', load: true, ignore: false},
+      {pattern: 'server.js', instrument: false,load: false, ignore: false},
+      {pattern: 'lib/**', load: true, ignore: false},
+      {pattern: 'routes/**/**/**/**', load: true, ignore: false},
+      {pattern: 'models/**/**', instrumented: false, load: true, ignore: false}
     ],
 
     tests: [
-      'test/**/*Spec.js'
+      'test/api/auditlog.spec.js'
+
     ],
 
-    setup: function () {
-      global.expect = require('chai').expect;
-    },
+    testFramework: 'jasmine@2.3.4',
 
+    workers: {
+      initial: 1,
+      regular: 1,
+      recycle: true
+    },
+    hints: {
+      ignoreCoverage: /ignore coverage/
+    },
+    // delays: {
+    //     edit: 500,
+    //     run: 150
+    // },
+    setup: function () {
+      var winston = require('winston');
+      if (!winston.transports.ConsoleLogger) {
+        var ConsoleLogger = winston.transports.ConsoleLogger = function (options) {
+          this.level = 'verbose';
+        };
+        require('util').inherits(ConsoleLogger, winston.Transport);
+        ConsoleLogger.prototype.log = function (level, msg, meta, callback) {
+          console.log(msg);
+          callback(null, true);
+        };
+        winston.remove(winston.transports.Console);
+        winston.add(winston.transports.ConsoleLogger);
+        winston.add = winston.remove = function () {
+
+        };
+      }
+
+    },
+    debug: true,
+    // TypeScript compiler is on by default with default the options,
+    // you can configure the built-in compiler by passing options to it.
+    // See interface CompilerOptions in
+    // https://github.com/Microsoft/TypeScript/blob/master/src/compiler/types.ts
+    // compilers: {
+    //'**/*.ts': wallaby.compilers.typeScript({}),
     env: {
+      // use 'node' type to use node.js
       type: 'node',
       runner: 'node'
+
     }
   };
 };
